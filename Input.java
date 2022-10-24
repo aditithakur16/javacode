49e7a0795c615a4f2a69990d1110ab2997dc0488
aditithakur16 <104360657+aditithakur16@users.noreply.github.com>
2022-10-24 17:45:05 +0530

    new

---
+++ Input.java
@@ -0,0 +1,11 @@
+import java.util.*;
+ class Input {
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        for(int i=1; i<=n; i++){
+          System.out.println(i);
+        }
+        System.out.println("Done");
+      }
+}
