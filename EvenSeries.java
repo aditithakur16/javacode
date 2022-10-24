59df6f02fae9ce8f7e38e816140e04c2d7df35a9
aditithakur16 <104360657+aditithakur16@users.noreply.github.com>
2022-10-24 17:48:05 +0530

    Evenseries

---
+++ EvenSeries.java
@@ -0,0 +1,11 @@
+public class EvenSeries {
+    public static void main(String [] args){
+        int i;
+        for(i=1; i<=100; i++){
+          if(i%2==0){
+            System.out.println(i);
+          }
+        }
+      }
+    
+}
