f42452bd9d6614716728c71eb8e386735c8d5f3d
aditithakur16 <104360657+aditithakur16@users.noreply.github.com>
2023-02-06 19:05:11 +0530

    s

---
+++ InputString.java
@@ -0,0 +1,10 @@
+package dailyJava;
+import java.util.Scanner;
+public class InputString {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String name = scn.nextLine();
+        System.err.println("Never Give Up "+ name +" :)");
+    }
+    
+}
