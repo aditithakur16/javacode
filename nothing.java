e90ca9d639246e88c0e38ab32db6fd4139f9d4a9
aditithakur16 <104360657+aditithakur16@users.noreply.github.com>
2023-04-06 23:00:49 +0530

    sun

--- nothing.java
+++ nothing.java
@@ -1,1 +1,10 @@
 //just for streak
+// another day without coad
+public class nothing {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 2;
+        int c = a+b;
+        System.out.println(c);
+    }
+}
