da61aab4725b6f4e91823502e3e45acab50bcf19
aditithakur16 <104360657+aditithakur16@users.noreply.github.com>
2023-04-24 23:44:20 +0530

    ...

--- nothing.java
+++ nothing.java
@@ -1,12 +1,15 @@
 //just for streak
 // another day without coad
+
 public class nothing {
+
     public static void main(String[] args) {
         int a = 5;
         int b = 2;
-        int c = a+b;
+        int c = a + b;
         System.out.println(c);
-        System.out.println(a-b);
-        System.out.println(a*b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(a / b);
     }
 }
