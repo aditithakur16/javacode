ebbf63ed3538429c5146b8ee9c47a0534fc33f6c
aditithakur16 <104360657+aditithakur16@users.noreply.github.com>
2023-05-26 22:40:53 +0530

    codeChef

---
+++ .vscode/JengaNight.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class JengaNight
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner scn = new Scanner(System.in);
+		int t = scn.nextInt();
+		for(int i = 1; i <= t; i++){
+		    int n = scn.nextInt();
+		    int x = scn.nextInt();
+		    if(n <= x && x%n==0){
+		        System.out.println("Yes");
+		    }else{
+		        System.out.println("No");
+		    }
+		}
+	}
+}
